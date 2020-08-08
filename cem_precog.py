f4b21d496897819299270916fa96568de0cbe9b1
Ubuntu <ubuntu@ip-172-31-15-184.us-east-2.compute.internal>
2020-08-08 01:25:05 +0000

    add different object positions

--- cem_precog.py
+++ cem_precog.py
@@ -14,7 +14,7 @@ import numpy as np
 import gym
 from precog.predictive_env import PredictiveModel
 import roboverse.bullet as bullet
-
+from datetime import datetime
 unnorm_data_path_server = "/nfs/kun1/users/huihanl/all.npy"
 unnorm_data_path_local = "/home/huihanl/bm-new/data/all_random_grasping_replayed/all.npy"
 
@@ -117,7 +117,7 @@ def get_elite_indicies(num_elite, returns, successes, only_success_elite):
     return indexes
 
 
-def create_env(randomize, reward_type):
+def create_env(randomize, reward_type, env_index):
 
     model_dir_local = "/home/huihanl/precog_nick/logs/esp_train_results/2020-07/" \
                 "07-23-19-52-51_dataset.sawyer_dataset_no_append.SawyerDatasetNoAppend_bijection" \
@@ -130,9 +130,38 @@ def create_env(randomize, reward_type):
     num_execution_per_step = 2
     single_obj_reward = 0
 
-    trimodal_positions = [(0.8187814400771692, 0.21049907010351596, -0.3415106684025205),
+    trimodal_positions6 = [(0.8187814400771692, 0.21049907010351596, -0.3415106684025205),
                           (0.739567302423451, 0.14341819851789023, -0.341380192135101),
                           (0.6694743281369817, 0.26676337932361205, -0.3440640126774397)]
+
+    trimodal_positions5 = [(0.6838513781345804, 0.13232607273283675, -0.34202212957838085), 
+                          (0.765710933262148, 0.17467922332524338, -0.3351583030520283), 
+                          (0.6789658820114347, 0.2548283056922718, -0.34103265910465325)]
+
+    trimodal_positions0 = [(0.7044727231056753, 0.2885882337328971, -0.3434554430497813), 
+                           (0.6950925950841189, 0.10769150255843486, -0.3408203519762647), 
+                           (0.8303787805809874, 0.12114265344994643, -0.34381720097869056)]
+
+    trimodal_positions1 = [(0.7543401984506464, 0.21230734165805112, -0.3400822998707757),
+                          (0.7250127303935135, 0.1172465013108163, -0.34050488361050935),
+                          (0.668601621056849, 0.20450849328681078, -0.34356676661067254)]
+                        
+    trimodal_positions2 = [(0.6623195604074853, 0.1420046836817064, -0.34036979015824237),
+                          (0.7490314063602679, 0.11150642565578632, -0.34010550517840776),
+                          (0.8313050761244212, 0.186704691256355, -0.3444288731959656)]
+                        
+    trimodal_positions3 = [(0.702806187582968, 0.282862951083425, -0.34329308543453),
+                          (0.8053413878164436, 0.15063075895870554, -0.34222136237585643),
+                          (0.6598757532001869, 0.10674964260605753, -0.34047814967568574)]
+                        
+    trimodal_positions4 = [(0.7935222824622936, 0.19097678049219627, -0.336295087280328),
+                           (0.6742503469035555, 0.1988108314637719, -0.3439745727367933),
+                           (0.6661682273867254, 0.09909463325237348, -0.3399316482536911)]
+
+    trimodal_positions_choice = [trimodal_positions0, trimodal_positions1, trimodal_positions2, 
+                                 trimodal_positions3, trimodal_positions4, trimodal_positions5, trimodal_positions6]
+
+    trimodal_positions = trimodal_positions_choice[env_index]
 
     base_env = roboverse.make(
         "SawyerGraspOneV2-v0", gui=False, randomize=randomize,
@@ -148,22 +177,45 @@ def create_env(randomize, reward_type):
 
     return env
 
+def generate_video(video_frames, savedir, index, fps=60):
+    assert fps == int(fps), fps
+    import skvideo.io
+    filename = os.path.join(savedir, "{}.mp4".format(index))
+    skvideo.io.vwrite(filename, video_frames, inputdict={'-r': str(int(fps))})
 
-def evaluate_z(z, randomize, reward_type):
-    env = create_env(randomize, reward_type)
+
+def evaluate_z(z, randomize, reward_type, output_dir, epoch, env_index):
+    env = create_env(randomize, reward_type, env_index)
     env.reset()
     rewards = []
     success = 0
+    images = []
     for i in range(12):
         z_action = z[i*4: (i+1)*4]
         next_observation, reward, done, info = env.step(z_action)
         rewards.append(reward)
+        images.append(env.render_obs())
         if done:
             if info["grasp_success"]:
                 success = 1
             break
     returns = sum(rewards)
+
+    """
+    if True: #epoch % 5 == 0:
+        video_save_dir = os.path.join(output_dir, "epoch_{}".format(epoch))
+        ensure_dir(video_save_dir)
+        now = datetime.now()
+        time_now = now.strftime("%m-%d-%H-%M-%S")
+        save_path_history = os.path.join(video_save_dir, time_now)
+        np.save(save_path_history, images)
+        #try:
+        #generate_video(images, video_save_dir, time_now)
+        #except:
+        #    pass
+    """
     return (returns, success)
+
 
 
 def run_cem(
@@ -176,6 +228,7 @@ def run_cem(
         randomize=True,
         only_success_elite=False,
         reward_type="sparse",
+        env_index=0,
 
         extra_std=2.0,
         extra_decay_time=10,
@@ -183,7 +236,11 @@ def run_cem(
         num_process=8
 ):
 
-    ensure_dir('./{}_small/'.format(env_id))
+    now = datetime.now()
+    time_now = now.strftime("%m-%d-%H-%M-%S")
+    output_dir = './{}/{}/'.format(env_id, time_now)
+    print("output_dir: ", output_dir)
+    ensure_dir(output_dir)
 
     start = time.time()
     num_episodes = epochs * num_process * batch_size
@@ -231,7 +288,8 @@ def run_cem(
         )
 
         with Pool(num_process) as p:
-            returns_successes = p.map(partial(evaluate_z, randomize=randomize, reward_type=reward_type), zs)
+            returns_successes = p.map(partial(evaluate_z, randomize=randomize, reward_type=reward_type, 
+                                              output_dir=output_dir, epoch=epoch, env_index=env_index), zs)
 
         print(returns_successes)
         returns = [rs[0] for rs in returns_successes]
@@ -281,21 +339,22 @@ def run_cem(
         if True: #epoch % 5 == 0:
             end = time.time()
             expt_time = end - start
-            plot_history(history, env_id, epoch, expt_time)
-            save_path_history = os.path.join('./{}_small/'.format(env_id), "history_{}.npy".format(epoch))
+            plot_history(history, output_dir, epoch, expt_time)
+            save_path_history = os.path.join(output_dir, "history_{}.npy".format(epoch))
             np.save(save_path_history, history)
-            save_path_elites = os.path.join('./{}_small/'.format(env_id), "elites_{}.npy".format(epoch))
+            save_path_elites = os.path.join(output_dir, "elites_{}.npy".format(epoch))
             np.save(save_path_elites, elites)
 
     end = time.time()
     expt_time = end - start
     print('expt took {:2.1f} seconds'.format(expt_time))
 
-    plot_history(history, env_id, epochs, expt_time)
+    plot_history(history, output_dir, epochs, expt_time)
     num_optimal = 5
     print('epochs done - evaluating {} best zs'.format(num_optimal))
 
-    best_z_rewards = [evaluate_z(z, randomize=randomize, reward_type=reward_type) for z in elites[:num_optimal]]
+    best_z_rewards = [evaluate_z(z, randomize=randomize, reward_type=reward_type, 
+                      output_dir=output_dir, epoch=epoch, env_index=env_index) for z in elites[:num_optimal]]
     print('best rewards - {} across {} samples'.format(best_z_rewards, num_optimal))
 
 
@@ -308,6 +367,8 @@ if __name__ == '__main__':
     parser.add_argument('--randomize', default=False, type=bool)
     parser.add_argument('--only_success_elite', default=False, type=bool)
     parser.add_argument('--reward_type', default="sparse", type=str)
+    parser.add_argument('--env_index', default=0, type=int)
+    
     args = parser.parse_args()
     print(args)
 
@@ -318,4 +379,5 @@ if __name__ == '__main__':
             batch_size=args.batch_size,
             only_success_elite=args.only_success_elite,
             reward_type=args.reward_type,
+            env_index=args.env_index,
             )
